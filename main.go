b5a2a7e6bceec36e43d91ad34f9bba474663c74f
Vladislav <vlad356102@yandex.ru>
2022-10-17 01:20:19 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func archiveString(s string) string {
+	//Счетчик символов в строке
+	symbolCounter := make(map[rune]int)
+	for _, r := range s {
+		symbolCounter[r] += 1
+	}
+
+	//Вытаскиваем ключи и конвертим руну к инту для сортировки по аски
+	runes := make([]int, 0, len(symbolCounter))
+	for r := range symbolCounter {
+		runes = append(runes, int(r))
+	}
+	sort.Ints(runes)
+
+	//Сб наш друг на длинные строки
+	sb := strings.Builder{}
+	for _, r := range runes {
+		sb.WriteString(fmt.Sprintf("%s%d", string(r), symbolCounter[rune(r)]))
+	}
+
+	return sb.String()
+}
+
+func main() {
+	println(archiveString("ЯЯЯБББддд"))
+	println(archiveString("aaabbbcccccaaaaa"))
+	println(archiveString("aaabbbccccc"))
+	println(archiveString("zzzzcccUUUuu"))
+}
