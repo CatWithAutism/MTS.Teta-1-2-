b52824d8e2a0ffbb725f8cdf90ed4d2c73953ee0
Vladislav <vlad356102@yandex.ru>
2022-10-17 01:59:53 +0300

    Update main.go

--- main.go
+++ main.go
@@ -23,14 +23,21 @@ func archiveString(s string) string {
 	//Сб наш друг на длинные строки
 	sb := strings.Builder{}
 	for _, r := range runes {
-		sb.WriteString(fmt.Sprintf("%s%d", string(r), symbolCounter[rune(r)]))
+		countOfSymbol := symbolCounter[rune(r)]
+		if countOfSymbol > 1 {
+			sb.WriteString(fmt.Sprintf("%s%d", string(r), countOfSymbol))
+		} else {
+			sb.WriteString(fmt.Sprintf("%s", string(r)))
+		}
+
 	}
 
 	return sb.String()
 }
 
 func main() {
-	println(archiveString("ЯЯЯБББддд"))
+	println(archiveString("ябд"))
+	println(archiveString("ЯЯЯБББддда"))
 	println(archiveString("aaabbbcccccaaaaa"))
 	println(archiveString("aaabbbccccc"))
 	println(archiveString("zzzzcccUUUuu"))
